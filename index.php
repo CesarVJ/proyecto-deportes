5a600a9885c045dc6d2c099a255b38da2cee37b6
cesar-vj <cvj991@gmail.com>
2020-10-08 00:23:09 -0500

    Se creo la estructura inicial del proyecto

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$inicio = "#";
+$registro = "registro.php";
+$login = "login.php";
+include_once("cabecera.html");
+include_once("menu.php");
+
+?>
+<main>
+    <div class="articulos">
+        <div class="articulo">
+
+        </div>
+        <div class="articulo">
+
+        </div>
+        <div class="articulo">
+
+        </div>
+        <div class="articulo">
+
+        </div>
+    </div>
+</main>
+<?php include_once("pie.html")?>
+</div>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<nav class="menu">
+    <a href="<?php echo $inicio;?>"><img class="icono" src="">  Inicio</a>
+    <a href="<?php echo $registro;?>"><img class="icono" src=""> Registrarse</a>
+    <a href="<?php echo $login;?>"><img class="icono" src=""> Iniciar Sesi&oacute;n</a>
+</nav>
