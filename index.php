1643222b166780d8386e3a1f112ac94acc3cc9ff
cesar-vj <cvj991@gmail.com>
2020-10-10 18:25:07 -0500

    Se agrego la seccion en el menu para filtrado por linea o por equipo

--- index.php
+++ index.php
@@ -8,9 +8,20 @@ include_once("menu.php");
 
 ?>
 <main>
+    <section class="filtro">
+        <form onsubmit="buscarProductos(); return false;">
+        <label for="tipoEquipo">Filtrar por equipo:</label>
+            <select id="tipoEquipo" name="tipoEquipo">
+            <option value=""></option>
+            </select>            
+            <input type="submit" value="Filtrar" />
+        </form>
+    </section>
     <div class="articulos">
         <div class="articulo">
-
+            <img src="./media/Balones/Adidas.jpg" alt="" class="imagen-articulo">
+            <h5 class="nombre-articulo"> Producto1</h5>
+            <button id="ver-articulo">Ver informaci&oacute;n</button>
         </div>
         <div class="articulo">
 
@@ -21,7 +32,9 @@ include_once("menu.php");
         <div class="articulo">
 
         </div>
-    </div>
+        <div class="articulo">
+
+        </div>
 </main>
 <?php include_once("pie.html")?>
 </div>
--- menu.php
+++ menu.php
@@ -1,5 +1,14 @@
 <nav class="menu">
     <a href="<?php echo $inicio;?>"><img class="icono" src="">  Inicio</a>
+    <div>
+    <a href="#"><img class="icono" src="">  Lineas</a>
+    <ul class="lineas">
+        <a href="<?php echo $inicio;?>" class="submenu"><img class="icono" src="">Uniformes</a>
+        <a href="<?php echo $inicio;?>" class="submenu"><img class="icono" src="">Ropa para Niños</a>
+        <a href="<?php echo $inicio;?>" class="submenu"><img class="icono" src="">Balones</a>
+        <a href="<?php echo $inicio;?>" class="submenu"><img class="icono" src="">Souvenirs</a>
+    </ul>
+    </div>
     <a href="<?php echo $registro;?>"><img class="icono" src=""> Registrarse</a>
     <a href="<?php echo $login;?>"><img class="icono" src=""> Iniciar Sesi&oacute;n</a>
 </nav>
